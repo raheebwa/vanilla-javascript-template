2732812faa66e1cf9ce0115f627422910e8735bf
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-04 23:16:49 +0300

    [fix] Not creating dist folder

--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ npm i -D stylelint-config-standard
 # 3. Create Directories and default files
 mkdir src
 mkdir dist
-touch src/main.js dist/index.html
+touch src/main.js 
+touch dist/index.html
 # 4. commit and push to origin
 git add .
 git commit -m "Setup project"
